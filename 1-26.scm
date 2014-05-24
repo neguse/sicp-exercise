b1b8b9a6eaaad82fe3a4f2779f3a76b6fe583cad
neguse <negcee@gmail.com>
2014-05-24 16:21:19 +0900

    solve 1.26

---
+++ 1-26.scm
@@ -0,0 +1,10 @@
+; http://en.wikipedia.org/wiki/Structure_and_Interpretation_of_Computer_Programs
+; Louis Reasonerさんは、どうもloose reasoner、つまりてきとーな人であると。
+; Evaさんは、まあEvaluatorですと。
+
+; Louisさんはてきとーなので、squareの引数のところが
+; 1個でも2個でも同じでしょ、どうせメモ化するよ、みたいに考えてそうな気がする。
+
+; さて、Louisさんの版だと、一回の再帰で2つ枝分かれするようになってしまうんですよ。
+; そうすると、直線的な再帰だったのが、三角形型になるんですね。
+; まあ、そんな感じだと思います。
