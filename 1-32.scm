fdbf6a18e95597da0358a9185a1abeb91ffc7ea6
neguse <negcee@gmail.com>
2014-06-09 00:40:12 +0900

    solve 1.32

---
+++ 1-32.scm
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+	null-value
+	(combiner (term a)
+			  (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate2 combiner null-value term a next b)
+  (define (iter a result)
+	(if (> a b)
+	  result
+	  (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+
+(define (sum term a next b)
+  (define (add x y) (+ x y))
+  (accumulate add 0 term a next b))
+
+; はい。
+
+(define (ident x) x)
+(define (inc x) (+ x 1))
+
+(sum ident 0 inc 10)
+
