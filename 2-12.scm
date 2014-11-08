19e2b8687af158f05ce992282cb8396139f5eb93
neguse <negcee@gmail.com>
2014-11-08 18:28:13 +0900

    solve 2.12

---
+++ 2-12.scm
@@ -0,0 +1,51 @@
+#lang racket
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+				 (- (upper-bound x) (lower-bound y))))
+
+(define (eq-interval x y)
+  (and (= (upper-bound x) (upper-bound y))
+	   (= (lower-bound x) (lower-bound y))))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+; percentって100%が1.0でいいんですよね
+
+(define (make-center-percent c p)
+  (make-center-width c (abs (* c p 0.01))))
+
+(define (percent i) (* 100.0 (/ (width i) (center i))))
+
+(= (percent (make-interval 9.0 11.0)) 10)
+(eq-interval (make-center-percent 10 10) (make-interval 9.0 11.0))
+
+; よし。
