ee0b3e78c87a3615331f1d599a752615f60c462b
neguse <negcee@gmail.com>
2014-11-29 18:34:30 +0900

    solve 2.39

---
+++ 2-39.scm
@@ -0,0 +1,26 @@
+#lang racket
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse-r sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reverse-l sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+(reverse-r '(1 2 3))
+(reverse-l '(1 2 3))
+
+; はい。
