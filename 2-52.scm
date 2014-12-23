7fce4037f88ed9c21e12eb45feb9eaef66ed865d
neguse <negcee@gmail.com>
2014-12-23 16:04:04 +0900

    solve 2.52

---
+++ 2-52.scm
@@ -0,0 +1,118 @@
+#lang racket
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+; http://www.billthelizard.com/2011/10/sicp-249-defining-primitive-painters.html
+
+(define wave-segments
+ (list
+  (make-segment
+   (make-vect 0.430 0.830)
+   (make-vect 0.460 0.830))
+  (make-segment
+   (make-vect 0.530 0.830)
+   (make-vect 0.570 0.830))
+  (make-segment
+   (make-vect 0.470 0.730)
+   (make-vect 0.530 0.730))
+  (make-segment
+   (make-vect 0.006 0.840)
+   (make-vect 0.155 0.591))
+  (make-segment
+   (make-vect 0.006 0.635)
+   (make-vect 0.155 0.392))
+  (make-segment
+   (make-vect 0.304 0.646)
+   (make-vect 0.155 0.591))
+  (make-segment
+   (make-vect 0.298 0.591)
+   (make-vect 0.155 0.392))
+  (make-segment
+   (make-vect 0.304 0.646)
+   (make-vect 0.403 0.646))
+  (make-segment
+   (make-vect 0.298 0.591)
+   (make-vect 0.354 0.492))
+  (make-segment
+   (make-vect 0.403 0.646)
+   (make-vect 0.348 0.845))
+  (make-segment
+   (make-vect 0.354 0.492)
+   (make-vect 0.249 0.000))
+  (make-segment
+   (make-vect 0.403 0.000)
+   (make-vect 0.502 0.293))
+  (make-segment
+   (make-vect 0.502 0.293)
+   (make-vect 0.602 0.000))
+  (make-segment
+   (make-vect 0.348 0.845)
+   (make-vect 0.403 0.999))
+  (make-segment
+   (make-vect 0.602 0.999)
+   (make-vect 0.652 0.845))
+  (make-segment
+   (make-vect 0.652 0.845)
+   (make-vect 0.602 0.646))
+  (make-segment
+   (make-vect 0.602 0.646)
+   (make-vect 0.751 0.646))
+  (make-segment
+   (make-vect 0.751 0.646)
+   (make-vect 0.999 0.343))
+  (make-segment
+   (make-vect 0.751 0.000)
+   (make-vect 0.597 0.442))
+  (make-segment
+   (make-vect 0.597 0.442)
+   (make-vect 0.999 0.144))))
+
+(define wave (segments->painter wave-segments))
+
+(define (split f1 f2)
+  (define (split-fn painter n)
+	(if (= n 0)
+	  painter
+	  (let ((smaller (split-fn painter (- n 1))))
+		(f1 painter (f2 smaller smaller)))))
+  split-fn)
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four identity flip-horiz
+                                  flip-vert rotate180)))
+    (combine4 (corner-split-mod painter n))))
+
+(define (corner-split-mod painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(paint wave)
+(paint (corner-split-mod wave 2))
+(paint (square-limit wave 2))
+
