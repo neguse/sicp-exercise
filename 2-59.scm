b4856f7797824c18e3cfdbcd0b79e0618ac01f96
neguse <negcee@gmail.com>
2015-03-15 16:33:55 +0900

    solve 2.59

---
+++ 2-59.scm
@@ -0,0 +1,22 @@
+#lang racket
+
+; ひさびさ。
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (union-set set1 set2)
+  (cond
+	((null? set1) set2)
+	((element-of-set? (car set1) set2)
+	 (union-set (cdr set1) set2))
+	(else (cons (car set1) (union-set (cdr set1) set2)))))
+
+(union-set '(2 3 4) '(1 2 3))
+; '(4 1 2 3)
+(union-set '(1 2 3) '(2 3 4))
+; '(1 2 3 4)
+
+; よし。
