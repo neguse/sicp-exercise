f0c35224491df48aa7eaf8921d597934f773ae66
neguse <negcee@gmail.com>
2015-03-29 20:59:40 +0900

    solve 2.63

---
+++ 2-63.scm
@@ -0,0 +1,75 @@
+#lang racket
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set) 
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+(define t1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+(define t2 '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ())))))
+(define t3 '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
+
+(display (tree->list-1 t1))
+(display "\n")
+(display (tree->list-2 t1))
+(display "\n")
+
+(display (tree->list-1 t2))
+(display "\n")
+(display (tree->list-2 t2))
+(display "\n")
+
+(display (tree->list-1 t3))
+(display "\n")
+(display (tree->list-2 t3))
+(display "\n")
+
+; ぱっと見同じっぽい
+
+; tree->list-1はappendでリスト結合しているので
+; 節をたどるごとにそこまでの要素数分だけのコストがかかりそう
+; tree->list-2はcopy-to-listでやってるんだけど、
+; こっちはconsで後ろにくっつけるだけだからオーダーは少なそう
+
+
