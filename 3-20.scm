365cab50b25dfcc8038348d87d567f26aa2c84a8
Ryuichi Kiriyama <negcee@gmail.com>
2015-08-01 21:36:30 +0900

    solve 3.20

---
+++ 3-20.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+; 図かくのめんどいので書きません。
+
+; (define x (cons 1 2))
+; x : (cons 1 2)
+
+; (define z (cons x x))
+; z : (cons x x)
+
+; (set-car! (cdr z) 17)
+; x : (cons 17 2)
+; z : (cons x x)
+
