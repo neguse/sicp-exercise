b91d7bc1a56277b575879f2502daf2c974402e68
neguse <negcee@gmail.com>
2016-04-29 17:04:37 +0900

    solve 4.53

---
+++ 4-53.scm
@@ -0,0 +1,25 @@
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+
+(define (prime-sum-pair list1 list2)
+  (let ((a (an-element-of list1))
+        (b (an-element-of list2)))
+    (require (prime? (+ a b)))
+    (list a b)))
+
+(let ((pairs '()))
+  (if-fail (let ((p (prime-sum-pair '(1 3 5 8) '(20 35 110))))
+             (permanent-set! pairs (cons p pairs))
+             (amb))
+           pairs))
+
+; ((8 35) (3 110) (3 20))
+; となりました。
+; これは、try-againしまくらなくても答えを列挙できるんですね。
+; これがもっとはやくあれば…
+
